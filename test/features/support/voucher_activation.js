677ba87adf2e702e0572f7fd5184e924d9aee5b7
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2023-01-09 14:21:47 +0100

    TECH-146:Fixed formatting in voucher activation test

--- test/features/support/voucher_activation.js
+++ test/features/support/voucher_activation.js
@@ -7,7 +7,7 @@ let specVoucherActivation;
 const baseUrl = `${localhost}vouchers/voucher_activation`;
 
 Before(() => {
-    specVoucherActivation = pactum.spec();
+  specVoucherActivation = pactum.spec();
 });
 
 // Background: The user wants to activate pre-activated voucher
@@ -20,13 +20,14 @@ Given(
 // Scenario: The user successfully activates a pre-activated voucher
 When(
   'The user triggers an action with a valid payload to activate a pre-activated voucher', () => {
-    specVoucherActivation
-      .patch(baseUrl)
-      .withBody({
-    "voucher_serial_number": 223,
-    "Gov_Stack_BB": "example Gov_Stack_BB"
-  });
-});
+  specVoucherActivation
+    .patch(baseUrl)
+    .withBody({
+      "voucher_serial_number": 223,
+      "Gov_Stack_BB": "example Gov_Stack_BB"
+    });
+  }
+);
 
 Then('The user successfully activates a pre-activated voucher', async () => {
   await specVoucherActivation.toss();
@@ -36,11 +37,10 @@ Then('The user successfully activates a pre-activated voucher', async () => {
 
 // Scenario: The user is not able to activate a pre-activated voucher because of an empty payload
 When(
-  'The user triggers an action with an empty payload to activate a pre-activated voucher',
-  () => {
-    specVoucherActivation
-      .patch(baseUrl)
-      .withBody();
+  'The user triggers an action with an empty payload to activate a pre-activated voucher',() => {
+  specVoucherActivation
+    .patch(baseUrl)
+    .withBody();
   }
 );
 
@@ -54,12 +54,12 @@ Then('The result of the operation returns an error because of empty payload', as
 // Scenario: The user is not able to activate a pre-activated voucher because of an invalid voucher serial number
 When(
   'The user triggers an action with an invalid serial number to activate a pre-activated voucher', () => {
-    specVoucherActivation
+  specVoucherActivation
     .patch(baseUrl)
     .withBody({
-    "voucher_serial_number": 0,
-    "Gov_Stack_BB": "example Gov_Stack_BB"
-});
+      "voucher_serial_number": 0,
+      "Gov_Stack_BB": "example Gov_Stack_BB"
+    });
   }
 );
   
@@ -71,12 +71,10 @@ Then('The result of the operation returns an error because of an invalid serial 
 
 // Scenario: The user is not able to activate a pre-activated voucher because of Gov Stack Building Block does not exist
 When(
-    'The user triggers an action with an invalid Gov Stack Building Block to activate a pre-activated voucher', () => {
-      specVoucherActivation
-      .patch(baseUrl)
-      .withBody({
-        "voucher_serial_number": 223
-    });
+  'The user triggers an action with an invalid Gov Stack Building Block to activate a pre-activated voucher', () => {
+  specVoucherActivation
+    .patch(baseUrl)
+    .withBody({"voucher_serial_number": 223});
   }
 );
     
@@ -87,5 +85,5 @@ Then('The result of the operation returns an error because of Gov Stack Building
 });
 
 After(() => {
-    specVoucherActivation.end();
+  specVoucherActivation.end();
 });
