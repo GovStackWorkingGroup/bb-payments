6c804266e00864e47b7f2094cce2b525771efb6c
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2023-01-16 15:31:36 +0100

    TECH-149:Fixed formatting, naming and made changes based on review for voucher cancelation

--- test/features/support/voucher_cancelation.js
+++ test/features/support/voucher_cancelation.js
@@ -4,12 +4,17 @@ const { localhost } = require('./helpers/helpers');
 
 let specVoucherCancelation;
 let voucherSerialNumber;
-let voucherCancelationBodyBody;
+let voucherCancelationBody = {
+  voucherserialnumber: 'exampleVoucherSerialNumber',
+  Gov_Stack_BB: 'Gov_Stack_BB',
+};
 
 const baseUrl = `${localhost}vouchers/voucherstatuscheck/{voucherserialnumber}`;
 const requestFunction = () =>
-  specVoucherCancelation.patch(baseUrl).withBody(voucherCancelationBodyBody)
-  .withPathParams('voucherserialnumber', voucherSerialNumber);
+  specVoucherCancelation
+    .patch(baseUrl)
+    .withBody(voucherCancelationBody)
+    .withPathParams('voucherserialnumber', voucherSerialNumber);
 
 Before(() => {
   specVoucherCancelation = pactum.spec();
@@ -18,95 +23,83 @@ Before(() => {
 // Scenario: The user successfully cancels a voucher
 Given('The user wants to cancel a voucher', () => {
   voucherSerialNumber = '1234';
-  voucherCancelationBodyBody = {
-    voucherserialnumber: voucherSerialNumber,
-    Gov_Stack_BB: 'Gov_Stack_BB'
-  };
-  return (voucherCancelationBodyBody,voucherSerialNumber);
+  voucherCancelationBody.voucherserialnumber = voucherSerialNumber;
+  return voucherCancelationBody, voucherSerialNumber;
 });
 
-When('The user triggers an action with a valid payload to cancel a voucher', () => {
-  requestFunction();
-});
+When(
+  'The user triggers an action with a valid payload to cancel a voucher',
+  () => {
+    requestFunction();
+  }
+);
 
 Then('The user successfully cancels the voucher', async () => {
   await specVoucherCancelation.toss();
   specVoucherCancelation.response().should.have.status(200);
   specVoucherCancelation.response().should.have.jsonLike({
-    'message': 'Voucher cancelled'
-  });
-});
-
-// Scenario: The user is not able to cancel the voucher without providing the voucher details
-Given('The user wants to cancel the voucher without providing the voucher details', () => {
-  voucherSerialNumber = '4321';
-  voucherCancelationBodyBody = null;
-  return (voucherCancelationBodyBody,voucherSerialNumber);
-});
-
-When('The user triggers an action to cancel the voucher without providing the voucher details', () => {
-  requestFunction();
-});
-
-Then('The result of an operation returns an error because of not providing the voucher details', async () => {
-  await specVoucherCancelation.toss();
-  specVoucherCancelation.response().should.have.status(400);
-  specVoucherCancelation.response().should.have.jsonLike({
-    'message': 'Invalid request'
+    message: 'Voucher cancelled',
   });
 });
 
 // Scenario: The user is not able to cancel the voucher with invalid voucher details
-Given('The user wants to cancel the voucher with invalid voucher details', () => {
-  voucherSerialNumber = '5432';
-  voucherCancelationBodyBody = {
-    voucherserialnumber: ''
-  };
-  return (voucherCancelationBodyBody,voucherSerialNumber);
-});
+Given(
+  'The user wants to cancel the voucher with invalid voucher details',
+  () => {
+    voucherSerialNumber = '5432';
+    voucherCancelationBody.voucherserialnumber = '';
+    return voucherCancelationBody, voucherSerialNumber;
+  }
+);
 
-When('The user triggers an action with invalid voucher details to cancel the voucher', () => {
-  requestFunction();
-});
+When(
+  'The user triggers an action with invalid voucher details to cancel the voucher',
+  () => {
+    requestFunction();
+  }
+);
 
-Then('The result of an operation returns an error because of providing invalid voucher details', async () => {
-  await specVoucherCancelation.toss();
-  specVoucherCancelation.response().should.have.status(400);
-  specVoucherCancelation.response().should.have.jsonLike({
-    'message': 'Invalid request'
-  });
-});
+Then(
+  'The result of an operation returns an error because of providing invalid voucher details',
+  async () => {
+    await specVoucherCancelation.toss();
+    specVoucherCancelation.response().should.have.status(400);
+    specVoucherCancelation.response().should.have.jsonLike({
+      message: 'Invalid request',
+    });
+  }
+);
 
 // Scenario: The user is not able to cancel the invalid voucher
 Given('The user wants to cancel the invalid voucher', () => {
   voucherSerialNumber = '3214';
-  voucherCancelationBodyBody = {
-    voucherserialnumber: voucherSerialNumber,
-    Gov_Stack_BB: 'Gov_Stack_BB'
-  };
-  return (voucherCancelationBodyBody,voucherSerialNumber);
+  voucherCancelationBody.voucherserialnumber = voucherSerialNumber;
+  return voucherCancelationBody, voucherSerialNumber;
 });
 
-When('The user triggers an action with a valid payload to cancel the invalid voucher', () => {
-  requestFunction();
-});
+When(
+  'The user triggers an action with a valid payload to cancel the invalid voucher',
+  () => {
+    requestFunction();
+  }
+);
 
-Then('The result of an operation returns an error because of the invalid voucher', async () => {
-  await specVoucherCancelation.toss();
-  specVoucherCancelation.response().should.have.status(463);
-  specVoucherCancelation.response().should.have.jsonLike({
-    'message': 'Invalid voucher'
-  });
-});
+Then(
+  'The result of an operation returns an error because of the invalid voucher',
+  async () => {
+    await specVoucherCancelation.toss();
+    specVoucherCancelation.response().should.have.status(463);
+    specVoucherCancelation.response().should.have.jsonLike({
+      message: 'Invalid voucher',
+    });
+  }
+);
 
 // Scenario: The user is not able to cancel the already canceled voucher
 Given('The user wants to cancel the already canceled voucher', () => {
   voucherSerialNumber = '2134';
-  voucherCancelationBodyBody = {
-    voucherserialnumber: 'voucherSerialNumber',
-    Gov_Stack_BB: 'Gov_Stack_BB'
-  };
-  return (voucherCancelationBodyBody,voucherSerialNumber);
+  voucherCancelationBody.voucherserialnumber = voucherSerialNumber;
+  return voucherCancelationBody, voucherSerialNumber;
 });
 
 When(
@@ -116,13 +109,44 @@ When(
   }
 );
 
-Then('The result of an operation returns an error with Voucher already canceled message', async () => {
-  await specVoucherCancelation.toss();
-  specVoucherCancelation.response().should.have.status(464);
-  specVoucherCancelation.response().should.have.jsonLike({
-    'message': 'Voucher is already cancelled'
-  });
-});
+Then(
+  'The result of an operation returns an error with Voucher already canceled message',
+  async () => {
+    await specVoucherCancelation.toss();
+    specVoucherCancelation.response().should.have.status(464);
+    specVoucherCancelation.response().should.have.jsonLike({
+      message: 'Voucher is already cancelled',
+    });
+  }
+);
+
+// Scenario: The user is not able to cancel the voucher without providing the voucher details
+Given(
+  'The user wants to cancel the voucher without providing the voucher details',
+  () => {
+    voucherSerialNumber = '4321';
+    voucherCancelationBody = null;
+    return voucherCancelationBody, voucherSerialNumber;
+  }
+);
+
+When(
+  'The user triggers an action to cancel the voucher without providing the voucher details',
+  () => {
+    requestFunction();
+  }
+);
+
+Then(
+  'The result of an operation returns an error because of not providing the voucher details',
+  async () => {
+    await specVoucherCancelation.toss();
+    specVoucherCancelation.response().should.have.status(400);
+    specVoucherCancelation.response().should.have.jsonLike({
+      message: 'Invalid request',
+    });
+  }
+);
 
 After(() => {
   specVoucherCancelation.end();
--- test/features/voucher_cancelation.feature
+++ test/features/voucher_cancelation.feature
@@ -8,11 +8,6 @@ Feature: The VoucherCancelation API is used to cancel a voucher
     When The user triggers an action with a valid payload to cancel a voucher
     Then The user successfully cancels the voucher
 
-  Scenario: The user is not able to cancel the voucher without providing the voucher details
-    Given The user wants to cancel the voucher without providing the voucher details
-    When The user triggers an action to cancel the voucher without providing the voucher details
-    Then The result of an operation returns an error because of not providing the voucher details
-
   Scenario: The user is not able to cancel the voucher with invalid voucher details
     Given The user wants to cancel the voucher with invalid voucher details
     When The user triggers an action with invalid voucher details to cancel the voucher
@@ -27,3 +22,8 @@ Feature: The VoucherCancelation API is used to cancel a voucher
     Given The user wants to cancel the already canceled voucher
     When The user triggers an action with a valid payload to cancel the voucher that has already been canceled
     Then The result of an operation returns an error with Voucher already canceled message
+
+  Scenario: The user is not able to cancel the voucher without providing the voucher details
+    Given The user wants to cancel the voucher without providing the voucher details
+    When The user triggers an action to cancel the voucher without providing the voucher details
+    Then The result of an operation returns an error because of not providing the voucher details
