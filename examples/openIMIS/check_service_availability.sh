01bd3779d58568f889bfed6faa6b809577cd08ab
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-25 15:19:13 +0200

    Added openIMIS Candidate application

---
+++ examples/openIMIS/check_service_availability.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+healthcheckApiCall() {
+    echo "Sending test request, result:"
+    echo "-----"
+    curl localhost:3333 > /dev/null
+    RETURN=$?
+    if [ $RETURN -ne 0 ];
+    then
+      echo "Api healthcheck call failed"
+      docker-compose logs backend
+      return 1
+    fi
+    echo "-----"
+    echo "Success in calling healtcheck API endpoint"
+    return 0
+}
+
+waitForAPI() {
+    echo "Testing API availability..."
+    retries=30
+    interval=10
+    notAvailable=1
+    while [ $retries -ge 0 ] && [ $notAvailable -ne 0 ]
+    do
+      healthcheckApiCall
+      notAvailable=$?
+      retries=$(( $retries - 1 ))
+      if [ $retries -ne 5 ]
+      then 
+         sleep 1
+      fi
+    done
+
+    if [ $notAvailable -eq 1 ]
+    then
+      echo "Gherkin tests will not be executed as API is not available."
+      exit 1
+    fi
+    echo "API Available."
+    return 0
+}
+
+waitForAPI
---
+++ examples/openIMIS/test_entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Ensure new version is used
+docker pull ghcr.io/openimis/openimis-be:govstack-api-test
+docker-compose build --no-cache db backend
+docker-compose run backend manage migrate
+
+docker-compose up -d db backend
+
+
+timeout=$((SECONDS + 120))
+until docker-compose exec backend echo "Service is up" || [ $SECONDS -gt $timeout ]; do
+    sleep 1
+done
+
+if [ $SECONDS -gt $timeout ]; then
+    echo "Timed out waiting for the backend service to start."
+    exit 1
+fi
+
+chmod u+x ./check_service_availability.sh && ./check_service_availability.sh
+docker-compose logs backend
+docker-compose exec backend python /createTestCustomParameters.py
+container_id=$(docker-compose ps -q backend)
+# docker cp $container_id:/openimis-be/openIMIS/testCustomParameters.json ./testCustomParameters.json
+
+docker-compose exec backend python /import_data.py
