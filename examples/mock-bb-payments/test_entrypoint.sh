fa1c411a4786e852d998cec65593ead8fbf52b33
Manoj <56669674+fynmanoj@users.noreply.github.com>
2023-12-05 14:14:24 +0530

    Update test_entrypoint.sh

--- examples/mock-bb-payments/test_entrypoint.sh
+++ examples/mock-bb-payments/test_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo $AWS_SECRET_ACCESS_KEY
 echo $AWS_REGION
+echo $AWS_CIRCLECI_ROLE_ARN
 cat  /home/circleci/parameters.json
 chmod u+x entrypoint.sh
 docker-compose up -d --build test-app
